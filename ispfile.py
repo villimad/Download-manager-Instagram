3e0e25767650a6f5b50f60805bfb57c0211f5818
Artem Levin <48175579+villimad@users.noreply.github.com>
2019-07-31 11:54:05 +0300

    Add files via upload
    Main class.

---
+++ ispfile.py
@@ -0,0 +1,24 @@
+from first import InstDWIMG
+from pars_page import Profile
+
+with open('profile.txt') as f:
+    mylist = [line.rstrip('\n') for line in f]
+
+p = len(mylist)
+for q1 in range(0, p, 1):
+    g = mylist[q1]
+    One = Profile(g)
+    d = One.Pars()
+    k = len(d)
+    for q2 in range(0, k, 1):
+        r = d[q2]
+        r = r[:-1]
+        r = r[1:]
+        two = InstDWIMG(r)
+        two.dw()
+        pass
+    pass
+
+
+
+
